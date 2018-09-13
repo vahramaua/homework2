aee72a9ec64bd9cae10cfc64efb4b5fa97867cbb
unknown <vahram_smbatyan@edu.aua.am>
2018-09-13 20:53:55 +0400

    first commit

---
+++ homework2.js
@@ -0,0 +1,16 @@
+alert("Vahram"+ " " + "Smbatyan");
+alert("Homework"+" "+"for"+" " + "CS"+ 110);
+const a=13 ;
+const b=12;
+const c=a*b;
+console.log(a+c);
+const age=18 ;
+const sadthing='Good bye university, Hello Army';
+if (age===18) {
+	console.log(sadthing) ;
+}
+if (c%2===1){
+	console.log("yuah")
+} else {
+	console.log("nuha")
+}
